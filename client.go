7e0c96b36dfdc9d9a8cf4cc4a7f14f90ea18601e
Krzysztof Szafrański <k.p.szafranski@gmail.com>
2018-11-24 15:34:43 +0200

    Add path-trace endpoint

---
+++ client.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"bytes"
+	"crypto/tls"
+	"encoding/base64"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/kszafran/junction-2018/models"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"sync"
+	"time"
+)
+
+type H map[string]string
+
+const (
+	host = "82.203.192.245"
+	user = "developer"
+	pass = "Hackjunction2018"
+)
+
+var (
+	sharedToken string
+	mutex       = &sync.RWMutex{}
+)
+
+func init() {
+	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+}
+
+func refreshToken() {
+	auth := func() {
+		log.Println("[INFO] Refreshing authorization token...")
+		token, err := authenticate()
+		if err != nil {
+			log.Printf("[ERROR] failed to authenticate: %v\n", err)
+		}
+		mutex.Lock()
+		sharedToken = token
+		mutex.Unlock()
+		log.Println("[INFO] New token obtained!")
+	}
+
+	auth()
+	ticker := time.NewTicker(5 * time.Minute)
+	go func() {
+		for range ticker.C {
+			auth()
+		}
+	}()
+}
+
+func authenticate() (token string, err error) {
+	req, err := http.NewRequest("POST", "https://"+host+"/dna/system/api/v1/auth/token", nil)
+	if err != nil {
+		return
+	}
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", "Basic "+base64.StdEncoding.EncodeToString([]byte(user+":"+pass)))
+	var auth models.AuthenticationAPIResponse
+	err = send(req, &auth)
+	if err != nil {
+		return
+	}
+	return auth.Token, nil
+}
+
+func getToken() string {
+	mutex.RLock()
+	defer mutex.RUnlock()
+	return sharedToken
+}
+
+func get(path string, headers H, response interface{}) error {
+	req, err := http.NewRequest("GET", "https://"+host+"/dna/intent/api/v1"+path, nil)
+	if err != nil {
+		return err
+	}
+	req.Header.Add("X-Auth-Token", getToken())
+	for name, value := range headers {
+		req.Header.Add(name, value)
+	}
+	return send(req, response)
+}
+
+func post(path string, headers H, response interface{}, request interface{}) error {
+	body, err := json.Marshal(request)
+	if err != nil {
+		return err
+	}
+	req, err := http.NewRequest("POST", "https://"+host+"/dna/intent/api/v1"+path, bytes.NewReader(body))
+	if err != nil {
+		return err
+	}
+	req.Header.Add("X-Auth-Token", getToken())
+	req.Header.Add("Content-Type", "application/json")
+	for name, value := range headers {
+		req.Header.Add(name, value)
+	}
+	return send(req, response)
+}
+
+func send(req *http.Request, response interface{}) error {
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	if res.StatusCode >= 300 {
+		body, _ := ioutil.ReadAll(res.Body)
+		return errors.New(fmt.Sprintf("unexpected response code (%d): %s", res.StatusCode, string(body)))
+	}
+	defer res.Body.Close()
+	if response == nil {
+		return nil
+	}
+	return json.NewDecoder(res.Body).Decode(&response)
+}
--- main.go
+++ main.go
@@ -1,134 +1,87 @@
 package main
 
 import (
-	"crypto/tls"
-	"encoding/base64"
-	"encoding/json"
-	"errors"
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/kszafran/junction-2018/models"
 	"io/ioutil"
 	"log"
-	"net/http"
-	"sync"
-	"time"
-)
-
-type H map[string]string
-
-const (
-	host = "82.203.192.245"
-	user = "developer"
-	pass = "Hackjunction2018"
-)
-
-var (
-	sharedToken string
-	mutex       = &sync.RWMutex{}
 )
 
 func main() {
-	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
-
 	refreshToken()
-
 	r := gin.Default()
-
-	r.GET("/test", func(c *gin.Context) {
-		var health models.GetOverallNetworkHealthResponseResponse
-		err := get("/network-health?timestamp", H{"__runsync": "true", "__timeout": "60", "__persistbapioutput": "true"}, &health)
-		if err != nil {
-			c.String(500, "failed to get health: %v", err)
-			c.AbortWithError(500, err)
-			return
-		}
-		c.JSON(200, health)
-	})
-
-	r.POST("/sensor/:ip", func(c *gin.Context) {
-		body, err := ioutil.ReadAll(c.Request.Body)
-		if err != nil {
-			c.String(500, "failed to read request body: %v", err)
-			c.AbortWithError(500, err)
-			return
-		}
-		log.Printf("[INFO] Got sensor information from %s: %s\n", c.Param("ip"), body)
-	})
-
+	r.GET("/test", testHandler)
+	r.GET("/path-trace", pathTraceHandler)
+	r.POST("/sensor/:ip", sensorHandler)
 	err := r.Run()
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
-func refreshToken() {
-	auth := func() {
-		log.Println("[INFO] Refreshing authorization token...")
-		token, err := authenticate()
+func testHandler(c *gin.Context) {
+	var health models.GetOverallNetworkHealthResponseResponse
+	err := get("/network-health?timestamp", H{"__runsync": "true", "__timeout": "60", "__persistbapioutput": "true"}, &health)
+	if err != nil {
+		c.String(500, "failed to get health: %v", err)
+		c.Error(err)
+		return
+	}
+	c.JSON(200, health)
+}
+
+func pathTraceHandler(c *gin.Context) {
+	source := c.Query("source")
+	dest := c.Query("dest")
+	protocol := c.Query("protocol")
+	if source == "" {
+		c.String(400, "'source' query param missing")
+		return
+	}
+	if dest == "" {
+		c.String(400, "'target' query param missing")
+		return
+	}
+	if protocol == "" {
+		protocol = "TCP"
+	}
+	var result models.FlowAnalysisRequestResultOutput
+	err := post("/flow-analysis", nil, &result, &models.FlowAnalysisRequest{
+		Protocol:   protocol,
+		SourceIP:   source,
+		SourcePort: "80",
+		DestIP:     dest,
+		DestPort:   "80",
+	})
+	if err != nil {
+		c.String(500, "failed to initiate path trace: %v", err)
+		c.Error(err)
+		return
+	}
+	var trace models.PathResponseResult
+	status := "INPROGRESS"
+	for status == "INPROGRESS" {
+		err = get("/flow-analysis/"+result.Response.FlowAnalysisID, nil, &trace)
 		if err != nil {
-			log.Printf("[ERROR] failed to authenticate: %v\n", err)
+			c.String(500, "failed to get path trace results: %v", err)
+			c.Error(err)
+			return
 		}
-		mutex.Lock()
-		sharedToken = token
-		mutex.Unlock()
-		log.Println("[INFO] New token obtained!")
+		status = trace.Response.Request.Status
 	}
-
-	auth()
-	ticker := time.NewTicker(5 * time.Minute)
-	go func() {
-		for range ticker.C {
-			auth()
-		}
-	}()
-}
-
-func authenticate() (token string, err error) {
-	req, err := http.NewRequest("POST", "https://"+host+"/dna/system/api/v1/auth/token", nil)
-	if err != nil {
+	if status != "COMPLETED" {
+		c.String(500, "path trace %s: %s", status, trace.Response.Request.FailureReason)
 		return
 	}
-	req.Header.Add("Content-Type", "application/json")
-	req.Header.Add("Authorization", "Basic "+base64.StdEncoding.EncodeToString([]byte(user+":"+pass)))
-	var auth models.AuthenticationAPIResponse
-	err = send(req, &auth)
+	c.JSON(200, trace.Response.NetworkElementsInfo)
+}
+
+func sensorHandler(c *gin.Context) {
+	body, err := ioutil.ReadAll(c.Request.Body)
 	if err != nil {
+		c.String(500, "failed to read request body: %v", err)
+		c.Error(err)
 		return
 	}
-	return auth.Token, nil
-}
-
-func getToken() string {
-	mutex.RLock()
-	defer mutex.RUnlock()
-	return sharedToken
-}
-
-func get(path string, headers H, response interface{}) error {
-	req, err := http.NewRequest("GET", "https://"+host+"/dna/intent/api/v1"+path, nil)
-	if err != nil {
-		return err
-	}
-	req.Header.Add("X-Auth-Token", getToken())
-	for name, value := range headers {
-		req.Header.Add(name, value)
-	}
-	return send(req, response)
-}
-
-func send(req *http.Request, response interface{}) error {
-	res, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return err
-	}
-	if res.StatusCode >= 300 {
-		body, _ := ioutil.ReadAll(res.Body)
-		return errors.New(fmt.Sprintf("unexpected response code (%d): %s", res.StatusCode, string(body)))
-	}
-	defer res.Body.Close()
-	if response == nil {
-		return nil
-	}
-	return json.NewDecoder(res.Body).Decode(&response)
+	log.Printf("[INFO] Got sensor information from %s: %s\n", c.Param("ip"), body)
 }
