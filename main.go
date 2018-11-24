ea513531f4490c26e0017d4e63904be58202f726
Krzysztof Szafrański <k.p.szafranski@gmail.com>
2018-11-24 11:22:08 +0200

    Refresh auth token periodically

--- main.go
+++ main.go
@@ -8,8 +8,11 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/kszafran/junction-2018/models"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"sync"
+	"time"
 )
 
 type H map[string]string
@@ -20,22 +23,23 @@ const (
 	pass = "Hackjunction2018"
 )
 
-// TODO refresh token
-var token string
+var (
+	sharedToken string
+	mutex       = &sync.RWMutex{}
+)
 
 func main() {
 	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+
+	refreshToken()
+
 	r := gin.Default()
 	r.GET("/test", func(c *gin.Context) {
-		err := authenticate()
-		if err != nil {
-			c.AbortWithError(500, errors.New(fmt.Sprintf("failed to authenticate: %v", err)))
-			return
-		}
 		var health models.GetOverallNetworkHealthResponseResponse
-		err = get("/network-health?timestamp", H{"__runsync": "true", "__timeout": "60", "__persistbapioutput": "true"}, &health)
+		err := get("/network-health?timestamp", H{"__runsync": "true", "__timeout": "60", "__persistbapioutput": "true"}, &health)
 		if err != nil {
-			c.AbortWithError(500, errors.New(fmt.Sprintf("failed to get health: %v", err)))
+			c.String(500, "failed to get health: %v", err)
+			c.AbortWithError(500, err)
 			return
 		}
 		c.JSON(200, health)
@@ -46,25 +50,47 @@ func main() {
 	}
 }
 
-func authenticate() error {
+func refreshToken() {
+	auth := func() {
+		log.Println("[INFO] Refreshing authorization token...")
+		token, err := authenticate()
+		if err != nil {
+			log.Printf("[ERROR] failed to authenticate: %v", err)
+		}
+		mutex.Lock()
+		sharedToken = token
+		mutex.Unlock()
+		log.Println("[INFO] New token obtained!")
+	}
+
+	auth()
+	ticker := time.NewTicker(5 * time.Minute)
+	go func() {
+		for range ticker.C {
+			auth()
+		}
+	}()
+}
+
+func authenticate() (token string, err error) {
 	req, err := http.NewRequest("POST", "https://"+host+"/dna/system/api/v1/auth/token", nil)
 	if err != nil {
-		return err
+		return
 	}
 	req.Header.Add("Content-Type", "application/json")
 	req.Header.Add("Authorization", "Basic "+base64.StdEncoding.EncodeToString([]byte(user+":"+pass)))
-	res, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return err
-	}
-	defer res.Body.Close()
 	var auth models.AuthenticationAPIResponse
-	err = json.NewDecoder(res.Body).Decode(&auth)
+	err = send(req, &auth)
 	if err != nil {
-		return err
+		return
 	}
-	token = auth.Token
-	return nil
+	return auth.Token, nil
+}
+
+func getToken() string {
+	mutex.RLock()
+	defer mutex.RUnlock()
+	return sharedToken
 }
 
 func get(path string, headers H, response interface{}) error {
@@ -72,14 +98,25 @@ func get(path string, headers H, response interface{}) error {
 	if err != nil {
 		return err
 	}
-	req.Header.Add("X-Auth-Token", token)
+	req.Header.Add("X-Auth-Token", getToken())
 	for name, value := range headers {
 		req.Header.Add(name, value)
 	}
+	return send(req, response)
+}
+
+func send(req *http.Request, response interface{}) error {
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return err
 	}
+	if res.StatusCode >= 300 {
+		body, _ := ioutil.ReadAll(res.Body)
+		return errors.New(fmt.Sprintf("unexpected response code (%d): %s", res.StatusCode, string(body)))
+	}
 	defer res.Body.Close()
+	if response == nil {
+		return nil
+	}
 	return json.NewDecoder(res.Body).Decode(&response)
 }
